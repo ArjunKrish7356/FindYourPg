acedd2a67a2ec39c51397dfd3b991eab0e0da007
M Arjun Krishna <119443675+ArjunKrish7356@users.noreply.github.com>
2023-05-19 16:42:59 +0530

    Add files via upload

---
+++ src/pages/dashboard/Dashboard.jsx
@@ -0,0 +1,27 @@
+import React, { useContext } from "react";
+import "./Dashboard.css";
+import logo from '../../images/logo.png'
+import { AuthContext } from "../../context";
+
+export default function Dashboard() {
+  const { user } = useContext(AuthContext);
+  console.log(user);
+  return (
+    <div className="dashboard">
+      <div className="nav">
+        <div className="nav-left">
+          <img src={logo} alt=""></img>
+        </div>
+        <div className="nav-right">
+          <p>{user.displayName}</p>
+          <img src="" alt=""></img>
+        </div>
+      </div>
+      <div className="dashboard-cont">
+        <div className="side-bar"></div>
+        <div className="main-cont"></div>
+        <div className="right-bar"></div>
+      </div>
+    </div>
+  );
+}
